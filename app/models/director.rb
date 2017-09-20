f20894bb0a655a3c229fe8b98bacbb20fadde731
Mateusz Palyz <mateusz.palyz@netguru.pl>
2017-09-20 20:40:28 +0200

    add models validations

--- app/models/director.rb
+++ app/models/director.rb
@@ -1,3 +1,9 @@
 class Director < ApplicationRecord
   has_many :films
+
+  validates :first_name, presence: true, length: { maximum: 150 }
+  validates :last_name, presence: true, length: { maximum: 150 }
+  validates :birthday, presence: true
+  validates :bio, presence: true, length: { maximum: 1000 }
+  validates :nationality, presence: true, length: { maximum: 150 }
 end
--- app/models/film.rb
+++ app/models/film.rb
@@ -1,4 +1,8 @@
 class Film < ApplicationRecord
   belongs_to :user
   belongs_to :director
+
+  validates :title, presence: true, length: { maximum: 150 }
+  validates :description, presence: true, length: { maximum: 1000 }
+  validates :released_at, presence: true
 end
